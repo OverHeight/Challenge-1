03d9533b385afcf4e754e0dedb635e4f3be4893c
OverHeight <109268126+OverHeight@users.noreply.github.com>
2023-04-19 18:49:45 +0700

    Add files via upload

---
+++ scripts/sortCarByYearDecendingly.js
@@ -0,0 +1,22 @@
+function sortCarByYearDescendingly(cars) {
+  // Sangat dianjurkan untuk console.log semua hal hehe
+  console.log(cars);
+
+  // Clone array untuk menghindari side-effect
+  // Apa itu side effect?
+  const result = [...cars];
+
+  // Tulis code-mu disini
+    for (var i = 0; i < cars.length - 1; i++) {
+      for (var j = 0; j < cars.length - i - 1; j++) {
+          if (cars[j].year < cars[j + 1].year) {
+              var temp = cars[j];
+              cars[j] = cars[j + 1];
+              cars[j + 1] = temp;
+          }
+      }
+  }
+  console.log(cars);
+  // Rubah code ini dengan array hasil sorting secara descending
+  return result;
+}
